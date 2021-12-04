05d49c3df51abd1c966cd1179b59d00455d8c0ea
Jamie Hale <jamiesonhale@gmail.com>
2021-12-03 23:06:41 -0500

    Tweaking

--- 3/python/part2.py
+++ 3/python/part2.py
@@ -3,15 +3,9 @@ import sys
 def bit_value(n, bit):
     return (n & (1 << bit)) >> bit
 
-def get_value_to_match(readings, bit, value_fn):
-    count = 0
-    for reading in readings:
-        if bit_value(reading, bit) == 1:
-            count += 1
-    return value_fn(readings, count)
-
-def get_filtered_readings(readings, bit, value_fn):
-    value_to_match = get_value_to_match(readings, bit, value_fn)
+def get_filtered_readings(readings, bit, get_value_to_match_fn):
+    ones = list(filter(lambda reading: bit_value(reading, bit) == 1, readings))
+    value_to_match = get_value_to_match_fn(len(readings), len(ones))
     return list(filter(lambda reading: bit_value(reading, bit) == value_to_match, readings))
 
 def determine_rating(readings, bit, value_fn):
@@ -25,21 +19,21 @@ def determine_rating(readings, bit, value_fn):
     )
 
 width = 12
-readings = []
+all_readings = []
 
 for line in sys.stdin:
-    readings.append(int(line, 2))
+    all_readings.append(int(line, 2))
 
 oxygen_generator_rating = determine_rating(
-    list(readings),
+    all_readings,
     width - 1,
-    lambda readings, count: 1 if count >= len(readings) - count else 0
+    lambda total, x: 1 if x >= total - x else 0
 )
 
 co2_scrubber_rating = determine_rating(
-    list(readings),
+    all_readings,
     width - 1,
-    lambda readings, count: 1 if count < len(readings) - count else 0
+    lambda total, x: 1 if x < total - x else 0
 )
 
 print(oxygen_generator_rating)
